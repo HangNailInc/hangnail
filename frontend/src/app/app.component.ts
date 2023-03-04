6532e8a68f9de9b7b0777d2aef82459b7e202ab7
Drew Bonnett <andrewbonnett@gmail.com>
2023-03-03 20:17:02 -0700

    app-component access to grid and colorpicker components

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { AfterViewInit, Component, Directive, Input, ViewChild } from '@angular/core';
-import { GridComponent } from './grid/grid.component';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { GridComponent, Tile } from './grid/grid.component';
 import { ColorPickerComponent } from './color-picker/color-picker.component';
 
 @Component({
@@ -7,17 +7,17 @@ import { ColorPickerComponent } from './color-picker/color-picker.component';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent implements AfterViewInit {
   title = 'app';
-  grid?: GridComponent;
-  picker?: ColorPickerComponent;
-
-  constructor() {
-    console.log(this.grid);
-  }
+  @ViewChild(GridComponent) grid!: GridComponent;
+  @ViewChild(ColorPickerComponent) colorPicker!: ColorPickerComponent;
 
   ngAfterViewInit() {
     // viewChildren
+    console.log("Hello world");
+    console.log(this.grid);
+    console.log(this.colorPicker);
   }
 
 }
